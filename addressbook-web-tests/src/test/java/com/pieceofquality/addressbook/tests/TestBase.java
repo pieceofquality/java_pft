99dbd2be2f1d04b2e6c154bf31475de1ea205207
Kirill Remizov <pieceofquality@gmail.com>
2016-09-21 04:41:30 +0300

    -add logger

--- addressbook-web-tests/src/test/java/com/pieceofquality/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/com/pieceofquality/addressbook/tests/TestBase.java
@@ -6,46 +6,54 @@ import com.pieceofquality.addressbook.model.Contacts;
 import com.pieceofquality.addressbook.model.GroupData;
 import com.pieceofquality.addressbook.model.Groups;
 import org.openqa.selenium.remote.BrowserType;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@Listeners(MyTestListener.class)
 public class TestBase {
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
     protected static final ApplicationManager app
             = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
-    public void setUp() throws Exception {
+    public void setUp(ITestContext context) throws Exception {
         app.init();
+        context.setAttribute("app", app);
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + " with parameters " + Arrays.asList(p));
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void logTestStop(Method m) {
+        logger.info("Stop test " + m.getName());
+    }
 
     public void verifyGroupListInUI() {
         if (Boolean.getBoolean("verifyUI")) {
             Groups dbGroups = app.db().groups();
             Groups uiGroups = app.group().all();
-            assertThat(uiGroups, equalTo(dbGroups.stream()
-                    .map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
-                    .collect(Collectors.toSet())));
-        }
-    }
-
-    public void verifyContactListUI() {
-        if (Boolean.getBoolean("verifyUI")) {
-            Contacts dbContact = app.db().contacts();
-            Contacts uiContact = app.contact().all();
-            assertThat(uiContact, equalTo(dbContact.stream().map((c) -> new ContactData()
-                    .withId(c.getId()).withFirstName(c.getFirstName()).withLastName(c.getLastName()))
+            assertThat(uiGroups, equalTo(dbGroups
+                    .stream().map((g) -> new GroupData().withId(g.getId()).withName(g.getName()))
                     .collect(Collectors.toSet())));
         }
     }
